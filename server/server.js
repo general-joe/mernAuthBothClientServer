0d6ad7a4b9d5c967542a80f376a3357f005553e0
D-General <kwabijoe274@gmail.com>
2025-01-24 00:50:14 +0000

    Fixed some bugs in the backend codes and it was cors issue

--- server/server.js
+++ server/server.js
@@ -7,9 +7,9 @@ import "dotenv/config";
 //we import cookieParser
 import cookieParser from "cookie-parser";
 //import connectDB funtion from config directory
-import connectDB  from "./config/mongodb.js";
+import connectDB from "./config/mongodb.js";
 //We are importing authRouter here
-import authRouter  from "./routes/authRoutes.js";
+import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoute.js";
 //let's create a new express app
 const app = express();
@@ -18,12 +18,13 @@ const PORT = process.env.PORT || 4000;
 //we are using database function here or evoking or calling the connectDB function here
 connectDB();
 //middlewares
- 
+
+const allowedOrigins = ["http://localhost:5173"];
+
 app.use(express.json());
 app.use(cookieParser());
 //we use cors middleware to allow cross origin requests
-app.use(cors({ credentials: true }));
-
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 //This is get endpoint is here and API endpoint
 app.get("/", (req, res) => res.send("API working"));
